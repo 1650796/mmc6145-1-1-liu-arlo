ee4d88e28a0099d5ee1bdd2ccebaeab6c4376d8b
catsociety <aliu6876@gmail.com>
2024-05-20 01:04:50 -0400

    fixed image issue i think

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,9 +1,10 @@
 import { Link } from 'react-router-dom'
 import styles from '../styles/button.module.css'
+import heroImg from '../assets/doubleexposure.png'
 
 export default function Hero() {
     return (
-    <div className='hero'>
+    <div className='hero' style={{backgroundImage:`url(${heroImg})`}}>
         <h1>Arlo Liu</h1>
         <p>Does some stuff every now and then.</p>
         <button className={styles.button}>
--- src/components/project.jsx
+++ src/components/project.jsx
@@ -1,10 +1,12 @@
 import { Link } from 'react-router-dom'
 import styles from '../styles/button.module.css'
+import projOne from '../assets/p2.jpg'
+import projTwo from '../assets/p1.jpg'
 
 
 function Projectone() {
     return (
-        <div className='projectOne'>
+        <div className='projectOne' style={{backgroundImage:`url(${projOne})`}}>
             <h3>Full Brand Identity Guide</h3>
             <p>
                 Check out the brand identity guide I created.
@@ -19,7 +21,7 @@ function Projectone() {
 
 function Projecttwo() {
     return (
-        <div className='projectTwo'>
+        <div className='projectTwo' style={{backgroundImage:`url(${projTwo})`}}>
             <h3>Full-Stack Application</h3>
             <p>
                 Check out the full-stack application I built.
